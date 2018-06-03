9ca99a5cad6a5a4ddd4f2be9360367ad731f3e88
s123unny <s123unny@gmail.com>
2018-06-03 19:29:11 +0800

    Server init

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+var port = 9488;
+var path = require("path");
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var logger = require('morgan');
+var fs = require("fs");
+var game_start = require("./routes/controller");
+var model = null;
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'views')));
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname, '/views/index.html');
+});
+
+game_start(io, model);
+
+http.listen(port, function(){
+	console.log('listening on *:'+port);
+});
