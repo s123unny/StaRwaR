0d1d71637ef36c9314430160a482993939c73d8b
s123unny <s123unny@gmail.com>
2018-07-05 15:06:51 +0800

    Change chatting name

--- views/js/chatRoom.js
+++ views/js/chatRoom.js
@@ -9,7 +9,7 @@ function addMessage(msg,flag){
 	$('#messages').prepend($('<li class='+flag+' >').text(msg));
 	console.log(flag);
 }
-socket.on('chat_message',(msg,flag) => addMessage(msg,flag));
+socket.on('chatting',(msg,flag) => addMessage(msg,flag));
 $('#messageForm').keypress(function(e){
 	var keyCode = e.keyCode || e.which;
 	if (keyCode == '13'){
