fb8b2175286dc396b8dcb40b6305c302fb7d23d2
YM <b05902027@ntu.edu.tw>
2018-07-06 21:33:19 +0800

    add picture

--- views/js/view.js
+++ views/js/view.js
@@ -7,5 +7,19 @@ function login() {
 	password = $('#teamPassword').val();
 	$('#container').show();
 	$('#login').hide();
+	$('#side-form').hide();
 	socket.emit("login", playerId, playerName, password);
+	socket.on();
+}
+var a = 1;
+function list_clicker(){
+	console.log("list_clicker")
+	if (a == 1){
+		$('#side-form').show();
+		a = 0;
+	}
+	else{
+		$('#side-form').hide();
+		a = 1;
+	}
 }
