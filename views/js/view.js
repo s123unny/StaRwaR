654dab652b6b044e436e98913d92e42c163e2f70
YM <b05902027@ntu.edu.tw>
2018-07-08 10:31:27 +0800

    planet update

--- views/js/view.js
+++ views/js/view.js
@@ -1,5 +1,4 @@
 var socket = io();
-
 function login() {
 	console.log("here")
 	playerId = Number( $('#teamID').val() );
@@ -8,6 +7,23 @@ function login() {
 	$('#container').show();
 	$('#login').hide();
 	$('#side-form').hide();
+	$('#planet-blue').hide();
+	$('#planet-red').hide();
+	$('#planet-orange').hide();
+	$("#m0").hide();
+	var planet_name = ['#m0','#m1','#m2','#m3','#m4','#m5','#m6','#m7','#m8','#m9'];
+
+	var arrayLength = planet_name.length;
+	console.log(arrayLength)
+	for (var i = 0; i < arrayLength; i++) {
+		console.log(planet_name[i]);
+		$(planet_name[i]).hide();
+    //Do something
+	}
+	document.getElementsByClassName('planet-blue')[0].style.visibility = 'hidden';
+	// $('#planet-unreached').show();
+
+
 	socket.emit("login", playerId, playerName, password);
 	socket.on();
 }
@@ -22,4 +38,8 @@ function list_clicker(){
 		$('#side-form').hide();
 		a = 1;
 	}
+}
+function new_star(star_id){
+	console.log(star_id + "hide")
+	document.getElementById(star_id).style.display = 'none';
 }
