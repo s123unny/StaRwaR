9fa08fefc1b008853c5fa014e1db0519cd299a83
吳宗翰 <patrickwu2@wuzonghande-MacBook-Pro.local>
2018-07-09 03:47:21 +0800

    Finish control panel

--- views/js/panel/mission_worker.js
+++ views/js/panel/mission_worker.js
@@ -12,7 +12,7 @@ var stars = {
 	star8: {name: "m0", type: 'mine'}, 
 	star9: {name: "m0", type: 'mine'}, 
 	star10: {name: "m0", type: 'mine'}, 
-	star11: {name: "m0", type: 'computer'}, 
+	star11: {name: "m0", type: 'ai_center'}, 
 	star12: {name: "m0", type: 'ai_center'}, 
 	star13: {name: "m0", type: 'ai_center'}, 
 	star14: {name: "m0", type: 'ai_center'}, 
@@ -56,107 +56,115 @@ function render_target(row){
 }
 
 /* click button and assign work */
+
 function submit_mission(row){
-	if (confirm("Sure to assign work ?\nYou can't revise the submission if you confirm !")){
-		console.log("Sure to assing work");
-		/* check if it is ok */
-		var ship = document.getElementById('Ship').textContent;
-		console.log(ship);
-		// check ship
-		if (ship == 0){
-			alert("You don't have any ships.");
-			return;
+        $.confirm({
+	    'title'     : 'Assign Confirmation',
+	    'message'   : 'You are about to assign work. <br />It cannot be modified further after comfirmation!',
+	    'buttons'   : {
+		'Yes'   : {
+		    'class' : 'yes_option',
+		    'action': function(){freeze_submit_block(row);}
+		},
+		'No'    : {
+		    'class' : 'no_option',
+		    'action': function(){console.log('Cancel submission.');}
 		}
-
-		// get value
-		var miner = Number($('#assign_mblock_slot'+row+'_M').val());
-		var trainer = Number($('#assign_mblock_slot'+row+'_T').val());
-		var hacker = Number($('#assign_mblock_slot'+row+'_H').val());
-		// invalid request
-		
-		if (miner < 0 || hacker < 0 || trainer < 0){
-			alert("This is feature <3 ");
-			return;
-		}
-		var type_get = document.getElementById('assign_mblock_slot'+row+'_type');
-		var carry_get = document.getElementById('assign_mblock_slot'+row+'_carry');
-		var type = type_get.options[type_get.selectedIndex].value;
-		var carry = carry_get.options[carry_get.selectedIndex].value;
-
-		if (trainer > $('#standby_trainer_num').text()){
-			alert("You don't have enough trainer !");
-			return;
-		}
-		if (miner > $('#standby_miner_num').text()){
-			alert("You don't have enough miner !");
-			return;
-		}
-		if (hacker > $('#standby_hacker_num').text()){
-			alert("You don't have enough hacker !");
-			return;
-		}
-		if (type == 'ai_center' && carry == 'nothing'){
-			alert("You should carry dataset with you if you want to train model at AI Center.");
-			return;
-		}
-		
-		// fixed 
-		$('#assign_mblock_slot'+row+'_submit').attr('disabled', true);
-		$('#assign_mblock_slot'+row+'_type').attr('disabled', true);
-		$('#assign_mblock_slot'+row+'_target').attr('disabled', true);
-		$('#assign_mblock_slot'+row+'_carry').attr('disabled', true);
-		$('#assign_mblock_slot'+row+'_M').attr('disabled', true);
-		$('#assign_mblock_slot'+row+'_T').attr('disabled', true);
-		$('#assign_mblock_slot'+row+'_H').attr('disabled', true);
-		// append block on it
-		var iDiv = document.createElement('div');
-		iDiv.style.position="relative";
-		iDiv.style.marginTop="-29.5px";
-		//iDiv.style.zIndex='100'; 
-		iDiv.style.backgroundColor="#C53131";
-		iDiv.style.opacity=0.5;
-		iDiv.style.width="720px";
-		iDiv.style.height="28px";
-		iDiv.style.borderStyle="none";
-		iDiv.style.borderRadius="8px"
-		var cDiv = document.getElementById('assign_mblock_slot'+row);
-		cDiv.parentNode.append(iDiv);
-	}
-	else{ 
-		console.log("Cancel submission.");
-	}
+	    }
+	});
 }
+
+function freeze_submit_block(row){
+
+	console.log("Sure to assign work");
+	/* check if it is ok */
+	var ship = document.getElementById('Ship').textContent;
+	// check ship
+	if (ship == 0){
+		myalert("You don't have any ships !");
+		return;
+	}
+
+	// get value
+	var miner = Number($('#assign_mblock_slot'+row+'_M').val());
+	var trainer = Number($('#assign_mblock_slot'+row+'_T').val());
+	var hacker = Number($('#assign_mblock_slot'+row+'_H').val());
+	// invalid request
+	
+	if (miner < 0 || hacker < 0 || trainer < 0){
+		myalert("This is feature <3 ");
+		return;
+	}
+	var type_get = document.getElementById('assign_mblock_slot'+row+'_type');
+	var carry_get = document.getElementById('assign_mblock_slot'+row+'_carry');
+	var type = type_get.options[type_get.selectedIndex].value;
+	var carry = carry_get.options[carry_get.selectedIndex].value;
+
+	if (trainer > $('#standby_trainer_num').text()){
+		myalert("You don't have enough trainer !");
+		return;
+	}
+	if (miner > $('#standby_miner_num').text()){
+		myalert("You don't have enough miner !");
+		return;
+	}
+	if (hacker > $('#standby_hacker_num').text()){
+		myalert("You don't have enough hacker !");
+		return;
+	}
+	if (type == 'ai_center' && carry == 'nothing'){
+		myalert("Prepare some Datasets before training !");
+		return;
+	}
+	
+	// fixed 
+	$('#assign_mblock_slot'+row+'_submit').attr('disabled', true);
+	$('#assign_mblock_slot'+row+'_type').attr('disabled', true);
+	$('#assign_mblock_slot'+row+'_target').attr('disabled', true);
+	$('#assign_mblock_slot'+row+'_carry').attr('disabled', true);
+	$('#assign_mblock_slot'+row+'_M').attr('disabled', true);
+	$('#assign_mblock_slot'+row+'_T').attr('disabled', true);
+	$('#assign_mblock_slot'+row+'_H').attr('disabled', true);
+	// append block on it
+	var iDiv = document.createElement('div');
+	iDiv.style.position="relative";
+	iDiv.style.marginTop="-29.5px";
+	//iDiv.style.zIndex='100'; 
+	iDiv.style.backgroundColor="#C53131";
+	iDiv.style.opacity=0.5;
+	iDiv.style.width="720px";
+	iDiv.style.height="28px";
+	iDiv.style.borderStyle="none";
+	iDiv.style.borderRadius="8px"
+	var cDiv = document.getElementById('assign_mblock_slot'+row);
+	cDiv.parentNode.append(iDiv);
+}
+
 /* come back function */
 function comeback(row){
 	console.log("callback");
-	if (confirm("Sure to comeback ?\nYou can't revise the submission if you confirm !")){
-		// check valid or not
-		var state = $('#ongoing_mblock_slot'+row+'_State').val();
-		if (state == "Ongoing"){
-			alert("You can't call ongoing workers back !");
-			return;
-		}
-		else{
-			$('#ongoing_mblock_slot'+row+'_State').text('Back');
-			$("#ongoing_mblock_slot"+row+"_back").attr('disabled', true);
-					// append block on it
-			var iDiv = document.createElement('div');
-			iDiv.style.position="relative";
-			iDiv.style.marginTop="-25px";
-			//iDiv.style.zIndex='100'; 
-			iDiv.style.backgroundColor="#C53131";
-			iDiv.style.opacity=0.5;
-			iDiv.style.width="720px";
-			iDiv.style.height="28px";
-			iDiv.style.borderStyle="none";
-			iDiv.style.borderRadius="8px"
-			
-			var cDiv = document.getElementById('going_mblock_slot'+row);
-			cDiv.parentNode.append(iDiv);
-			return;
-		}
+
+	var state = document.getElementById('ongoing_mblock_slot'+row+'_State').textContent;
+	console.log(state);
+	if (state == "Ongoing"){
+		document.getElementById('ongoing_mblock_slot'+row+'_withdraw').checked = false;
+		alert("You can't call ongoing workers back !");
+		return;
 	}
 	else{
-		console.log("Cancel");
+		return;
 	}
+}
+
+
+function myalert(msg){
+        $.alert({
+	    'message'   : msg,
+	    'buttons'   : {
+		'Got It'   : {
+		    'class' : 'alert_box',
+		    'action': function(){}
+		}
+	    }
+	});
 }
