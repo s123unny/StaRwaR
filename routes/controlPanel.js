3762d12a085a267733a5c64a0c60d2eac3d3206b
Po-wen Chen <jacky87810@gmail.com>
2018-07-09 23:58:22 +0800

    Finish first step of render second_index

--- routes/controlPanel.js
+++ routes/controlPanel.js
@@ -3,10 +3,15 @@ var router = express.Router();
 var path = require('path');
 
 router.get('/:pwd', function(req, res) { //id = req.query.id
+  console.log(model.stars.list_from_type("unknown"))
   res.render('second_index.ejs', {
-    player_id:req.query.id,
-    money: model.players[req.query.id].money
-    
+    player: model.players[req.query.id],
+    ships: model.players[req.query.id].ships,
+    stars: model.stars,
+    mines: model.stars.list_from_type("mine"),
+    unknowns: model.stars.list_from_type("unknown"),
+    computers: model.stars.list_from_type("computer"),
+    abandons: model.stars.list_from_type("abandon"),
   })
 });
 
