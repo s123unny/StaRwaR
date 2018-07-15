ec851bd4b23d4bda9e569d96edf2e9b106ed2e49
吳宗翰 <patrickwu2@gmail.com>
2018-07-15 19:58:31 +0800

    fix XSS

--- views/js/chatRoom.js
+++ views/js/chatRoom.js
@@ -17,19 +17,20 @@ $('#messageForm').keypress(function(e){
 var counter = -1;
 var chatbox = ["","","","","","","","","",""];
 var chatcolor = ["white", "white", "white", "white", "white", "white", "white", "white", "white", "white"]
-var message_count = 20
+var message_count = 30
 //var chatcolor = ["aqua", "yellow", "orange", "pink", "violet", "lime", "salmon", "lime", "cyan", "chartreuse"]
 function addMessage(inner,name,color){
 	counter += 1;
 	console.log(inner);
 	var paragraph = document.getElementById("chat_msg");
 	if(counter < message_count){
-		chatbox[counter] = "["+ name + "]" + inner;
+		chatbox[counter] = "["+ name + "] " + inner;
 		chatcolor[counter] = color;
 		console.log(chatbox[counter]);
-		var s = "<ul class= 'chatroom-text' id = 'chatroom-text-"+counter+"'"+">" + "["+name+"] "+inner+"</ul>";
-		$('#chat_msg').append(s);
+		// var s = "<ul class= 'chatroom-text' id = 'chatroom-text-"+counter+"'"+">" + "["+name+"] "+inner+"</ul>";
+		// $('#chat_msg').append(s);
 		// paragraph.innerHTML += s;
+		$('#chatroom-text-'+counter).text(chatbox[counter]);
 		document.getElementById("chatroom-text-"+counter).style.color = chatcolor[counter];
 	}
 	else{
@@ -39,9 +40,9 @@ function addMessage(inner,name,color){
 			$("#chatroom-text-" + String(i)).text(chatbox[i+1]);
 			document.getElementById("chatroom-text-"+String(i)).style.color = chatcolor[i+1];
 		}
-		chatbox[message_count-1] = "["+ name + "]" + inner;
+		chatbox[message_count-1] = "["+ name + "] " + inner;
 		chatcolor[message_count-1] = color;
-		$("#chatroom-text-9").text(chatbox[message_count-1]);
-		document.getElementById("chatroom-text-9").style.color = chatcolor[message_count-1];
+		$("#chatroom-text-29").text(chatbox[message_count-1]);
+		document.getElementById("chatroom-text-29").style.color = chatcolor[message_count-1];
 	}
 }
